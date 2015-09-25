d879090e10337a3fd5ea134dfb343ce9e818db8f
wozgeass <wozgeass@github.com>
2015-09-25 16:39:04 -0500

    Ejemplo5

---
+++ codigo5.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+entero=10 # numero entero
+flotante=10.0 # numero decimal
+cadena="Ola Ke ase, pythoneando o ke ase" # una cadena
+Booleano=True # un simple booleano
+complejo=3+2j # numero complejo
+print("Este es un valor entero ->", entero)
+print("Este es un valor flotante ->", flotante)
+print("Este es un valor Cadena ->", cadena)
+print("Este es un valor Booleano ->", Booleano)
