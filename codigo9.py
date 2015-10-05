361158615e18a00a5da49b7831f2210714fa7cbe
wozgeass <wozgeass@github.com>
2015-10-05 12:38:06 -0500

    Ejemplo 9

---
+++ codigo9.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+entero=10 # numero entero
+flotante=10.0 # numero decimal
+cadena="Ola Ke ase, pythoneando o ke ase" # una cadena
+Booleano=True # un simple booleano
+complejo=3+2j # numero complejo
+caracter = "a"
+print("Este es un valor entero [ %f ] " % entero)
+print("Este es un valor flotante [ %d ]" % flotante)
+print("Este es un valor Cadena [ %s ]" % cadena)
+print("Este es un valor Booleano [ %s ]" % Booleano)
+print ("Es es un valor complejo [ %s ]" % complejo)
+print ("Caracter expresado en numero es [ %d ]" % ord(caracter))
