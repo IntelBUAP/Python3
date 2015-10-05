d5ca136ca7a5dafdb7b6dcb36fb6f3f270039620
wozgeass <wozgeass@github.com>
2015-10-05 12:38:58 -0500

    Ejemplo 10

---
+++ codigo10.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+# Calculo del perimetro de Un circulo
+PI = 3.14159265
+radio = 10
+print ("El radio del circulo es [ %d ] su perimetro es igual a [ %f.]" % ( radio, PI * ( radio ** 2 )))
+print ("El radio del circulo es [ %d ] su perimetro es igual a [ %.3f]" % ( radio, PI * ( radio ** 2 )))
