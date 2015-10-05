747369871ebbcfd1d1f5a84eb279b4364679f482
wozgeass <wozgeass@github.com>
2015-10-05 12:39:26 -0500

    Ejemplo 11

---
+++ codigo11.py
@@ -0,0 +1,14 @@
+# Ejemplos de print usando format()
+
+print ('{} {}'.format('uno', 'dos'))
+print ('{1} {0}'.format('uno', 'dos'))
+print ('{:>5}'.format('Hola mundo'))
+print ('{:15}'.format('Hola mundo'))
+print ('{:*<15}'.format('Hola mundo'))
+print ('{:^16}'.format('Hola mundo'))
+print ('{:.5}'.format('Hola mundo'))
+print ('{:.{}}'.format('Hola mundo',5))
+print ('{:d}'.format(25))
+print ('{:f}'.format(3.14159265358979))
+print ('{:3.3f}'.format(3.14159265358))
+
