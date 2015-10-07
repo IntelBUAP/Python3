1416897d5bb02ffa03b77336dbb661b7891a49cc
wozgeass <wozgeass@github.com>
2015-10-07 13:52:24 -0500

    Ejemplo 15

---
+++ codigo15.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+# Como hacen los gatos
+animal = "gato"
+if	animal == "gato":
+	print ("Los gatos hacen < Mau >")
+	print ("Ya que solo los gatos maullan")
+
+print ("fin del if")
