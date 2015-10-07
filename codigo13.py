e7ba53f530539b7e13234f7a4d45262187798907
wozgeass <wozgeass@github.com>
2015-10-07 13:35:43 -0500

    Ejemplo 13

---
+++ codigo13.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# Lectura de teclado usando eval
+numero = eval(input("Ingrese un numero ->\t"))
+decimal = eval(input("Ingrese un decimal ->\t"))
+cadena = input("Ingrese una cadena ->\t")
+complejo = eval(input("Ingrese un complejo ->\t"))
+print ("El tipo de Dato ingresado es [%d]" % numero)
+print ("El tipo de Dato ingresado es [%f]" % decimal)
+print ("El tipo de Dato ingresado es [%s]" % cadena)
+print ("El tipo de Dato ingresado es [%s]" % complejo)
