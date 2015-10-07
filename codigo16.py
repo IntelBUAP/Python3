48ad5885bb792acabe0755e955dfdb83a149f39d
wozgeass <wozgeass@github.com>
2015-10-07 13:52:46 -0500

    Ejemplo 16

---
+++ codigo16.py
@@ -0,0 +1,13 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+# Como hacen los gatos
+animal = input("Ingrese el sonido de animal que quiera > ")
+
+if	animal == "gato":
+	print ("Los gatos hacen < Mau >")
+	print ("Ya que solo los gatos maullan")
+
+else:
+	print ("No es un gato")
+	print ("=P")
+
