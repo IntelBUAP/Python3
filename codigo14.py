471827a4db6590d094a67f42eefc4a3ebae4eba7
wozgeass <wozgeass@github.com>
2015-10-07 13:36:57 -0500

    Ejemplo 14

---
+++ codigo14.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+
+# La iteracion con un diccionario.
+diccionario = {"adrian":26, "Alejandro":23, "Monica":23, "Windowsero":26}
+for clave, valor in diccionario.items():
+    print("El valor de la clave es {0} y el valor es {1}".format(clave,valor))
