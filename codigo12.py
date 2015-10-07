8dce430665244eb8fa7ad4ddad0f292ddf17a904
wozgeass <wozgeass@github.com>
2015-10-07 13:34:45 -0500

    Ejemplo 12

---
+++ codigo12.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*- coding:utf-8 -*-
+# Lectura de teclado
+numero=input("Ingrese un numero ->\t")
+decimal=input("Ingrese un decimal ->\t")
+cadena=input("Ingrese una cadena ->\t")
+complejo=input("Ingrese un complejo ->\t")
+print ("El tipo de Dato ingresado es [%d]" % int(numero))
+print ("El tipo de Dato ingresado es [%f]" % float(decimal))
+print ("El tipo de Dato ingresado es [%s]" % str(cadena))
+print ("El tipo de Dato ingresado es [%s]" % complejo)
